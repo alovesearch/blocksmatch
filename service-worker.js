a4e0797c1ad91e9d962d04540d83ddf68db8da40
alovesearch <91748042+alovesearch@users.noreply.github.com>
2024-10-04 04:53:28 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,14 +3,14 @@ self.addEventListener('install', (event) => {
         caches.open('my-pwa-cache')
         .then((cache) => {
             return cache.addAll([
-                '/blocksmatch/',
-                '/blocksmatch/index.html',
-                '/blocksmatch/u30.css',
-                '/blocksmatch/u32-2-A0.js',
-                '/blocksmatch/ss_bg4.webp',
-                '/blocksmatch/MyPWAServlet.java',
-                '/blocksmatch/manifest.json',
-                '/blocksmatch/service-worker.js',
+                '/',
+                '/index.html',
+                '/u30.css',
+                '/u32-2-A0.js',
+                '/ss_bg4.webp',
+                '/MyPWAServlet.java',
+                '/manifest.json',
+                '/service-worker.js',
                 '/blocksmatch/icons/icon-192x192.png',
                 '/blocksmatch/icons/icon-512x512.png'
             ]);
